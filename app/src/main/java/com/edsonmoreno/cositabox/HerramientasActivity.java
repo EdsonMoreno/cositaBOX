b59d9d5cc5169fadf331f7438b79f86343680723
Edson Moreno <diego2816@gmail.com>
2020-11-13 17:28:07 -0400

    Diseñado la interfaz previa de los fragments

--- app/src/main/java/com/edsonmoreno/cositabox/HerramientasActivity.java
+++ app/src/main/java/com/edsonmoreno/cositabox/HerramientasActivity.java
@@ -2,13 +2,36 @@ package com.edsonmoreno.cositabox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
-public class HerramientasActivity extends AppCompatActivity {
+public class HerramientasActivity extends AppCompatActivity implements  GestorMenu {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fragmentos_cargados = new Fragment[3];
+        fragmentos_cargados[0] = new LinternaFragment();
+        fragmentos_cargados[1] = new MusicaFragment();
+        fragmentos_cargados[2] = new NivelFragment();
+
         setContentView(R.layout.activity_herramientas);
+        //se averigua que boton hizo la llamada
+        Bundle extras = this.getIntent().getExtras();
+        this.menu(extras.getInt("boton_pulsado"));
+
     }
+
+    @Override
+    public void menu(int boton_pulsado) {
+        FragmentManager manejador = getFragmentManager();
+        FragmentTransaction ft = manejador.beginTransaction();
+        ft.replace(R.id.herramientas, fragmentos_cargados[boton_pulsado]);
+        ft.commit();
+    }
+
+    private Fragment fragmentos_cargados[];
 }
